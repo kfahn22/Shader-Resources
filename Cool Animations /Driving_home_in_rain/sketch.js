ede987bb0efc7ffe0e4686fbee0cdbd12b9feaee
kfahn22 <kfm722@gmail.com>
2022-04-09 07:28:43 -0400

    editing the Driving Home sketch

--- Cool Animations /Driving_home_in_rain/sketch.js
+++ Cool Animations /Driving_home_in_rain/sketch.js
@@ -1,28 +1,27 @@
-
-// Ported to P5.js from The Drive Home tutorial
+// Ported to P5.js from The Drive Home tutorials
 // by Martijn Steinrucken aka The Art of Code/BigWings 
 // https://www.youtube.com/watch?v=eKtsY7hYTPg
+// https://www.shadertoy.com/view/MdfBRX
 
-let textureImg;
+// I highly recommend watching the full tutorial series!
+// Note:  the original code uses multiple loops that iterate from 0., 1. and increment by a fraction.
+// This would not compile in p5.js, so I changed the interation paramenters.
+// The original version is MUCH better--take a look at the version in shadertoy to compare.
 
 // a shader variable
 let theShader;
 
 function preload(){
   // load the shader
-  // monochromatic image works best
-  textureImg = loadImage("Assets/ice.PNG");
   theShader = loadShader('starter.vert', 'starter.frag');
 }
-
 
 function setup() {
   pixelDensity(1);
   // shaders require WEBGL mode to work
-  createCanvas(800, 800, WEBGL);
+  createCanvas(840, 472, WEBGL);
   noStroke();
 }
-
 
 function draw() {  
   background(0);
@@ -32,13 +31,10 @@ function draw() {
   theShader.setUniform("iMouse", [mouseX, map(mouseY, 0, height, height, 0)]);
   theShader.setUniform("iFrame", frameCount);
   theShader.setUniform("iTime", millis()/1000.);
-  theShader.setUniform("tex0", textureImg);
   
   // shader() sets the active shader with our shader
   shader(theShader);
   //model(cubeObj);
   // rect gives us some geometry on the screen
   rect(0,0,width, height);
-  
 }
-
